40dfcee2cd22d1fffd79f4ec61c76b3704e083df
skphi13 <38512251+skphi13@users.noreply.github.com>
2018-04-19 16:24:44 -0500

    Add files via upload

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,29 +2,29 @@ import fresh_tomatoes
 import media
 
 walk_to_remember = media.Movie("A Walk to Remember",
-                        "Girl who change the life of a boy.",
-                        "http://www.gstatic.com/tv/thumb/movieposters/29274/p29274_p_v8_ae.jpg",
-                        "https://www.youtube.com/watch?v=EgdoQ8Oxu2E",
-                        "January 25, 2002")
+                               "Girl who change the life of a boy.",
+                               "http://www.gstatic.com/tv/thumb/movieposters/29274/p29274_p_v8_ae.jpg",
+                               "https://www.youtube.com/watch?v=EgdoQ8Oxu2E",
+                               "January 25, 2002")
 
 tokyo_drift = media.Movie("Tokyo Drift",
-                        "Drift racing in Japan.",
-                        "http://www.gstatic.com/tv/thumb/movieposters/159790/p159790_p_v8_ak.jpg",
-                        "https://www.youtube.com/watch?v=p8HQ2JLlc4E",
-                        "June 16, 2006")
+                          "Drift racing in Japan.",
+                          "http://www.gstatic.com/tv/thumb/movieposters/159790/p159790_p_v8_ak.jpg",
+                          "https://www.youtube.com/watch?v=p8HQ2JLlc4E",
+                          "June 16, 2006")
 
 the_foreigner = media.Movie("The Foreigner",
-                        "Father seeks vengence for the death of his daughter.",
-                        "http://t2.gstatic.com/images?q=tbn:ANd9GcSLrVta3GZy0KL1uMKRobMcjsNt3neibUJh2e8_I7T_gqNrRGO2",
-                        "https://www.youtube.com/watch?v=CQsRAYf8Cyk",
-                        "September 30, 2017")
+                            "Father seeks vengence for the death of his daughter.",
+                            "http://t2.gstatic.com/images?q=tbn:ANd9GcSLrVta3GZy0KL1uMKRobMcjsNt3neibUJh2e8_I7T_gqNrRGO2",
+                            "https://www.youtube.com/watch?v=CQsRAYf8Cyk",
+                            "September 30, 2017")
 
 
 blood_wars = media.Movie("Underworld: Blood Wars",
-                        "Selene vs the lycans.",
-                        "https://ia.media-imdb.com/images/M/MV5BMjI5Njk0NTIyNV5BMl5BanBnXkFtZTgwNjU4MjY5MDI@._V1_UX182_CR0,0,182,268_AL_.jpg",
-                        "https://www.youtube.com/watch?v=iT8w3gSa000",
-                        "January 6, 2017")
+                         "Selene vs the lycans.",
+                         "https://ia.media-imdb.com/images/M/MV5BMjI5Njk0NTIyNV5BMl5BanBnXkFtZTgwNjU4MjY5MDI@._V1_UX182_CR0,0,182,268_AL_.jpg",
+                         "https://www.youtube.com/watch?v=iT8w3gSa000",
+                         "January 6, 2017")
 
 john_wick = media.Movie("John Wick",
                         "Payback for killing his dog.",
@@ -38,9 +38,9 @@ civil_war = media.Movie("Captain America: Civil War",
                         "https://www.youtube.com/watch?v=mKScYV9jzG0",
                         "May 6, 2016")
 
-    # Store the Movie objects in a list.
+# Store the Movie objects in a list.
 movies = [walk_to_remember, tokyo_drift, the_foreigner, blood_wars, john_wick, civil_war]
 
-    # Open the movie website in the user's browser, featuring the movies above.
+# Open the movie website in the user's browser, featuring the movies above.
 fresh_tomatoes.open_movies_page(movies)
 
--- media.py
+++ media.py
@@ -1,9 +1,18 @@
 import webbrowser
 
+
 class Movie(object):
-    
+    " Class for movie trailer "
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube, movie_release_date):
+        """ Class describing the movie
+        Args:
+        title (str)= movie's title
+        storyline (str)= what the movie is about
+        poster_image_url (str)= URL to a poster image
+        trailer_youtube_url (str)= youtube video of the movie's trailer
+        release_date (str)= date the movie was release
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
